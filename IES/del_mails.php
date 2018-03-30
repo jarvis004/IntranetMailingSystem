9bdd679eb1101dcd26b230ac7bdb6db94ca82738
KamleshGupta123 <gkamal625@gmail.com>
2018-03-30 05:41:08 -0700

    Update del_mails.php

--- IES/del_mails.php
+++ IES/del_mails.php
@@ -7,16 +7,17 @@
 
 <body>
 <?php
-	$_POST['delete'];
+	/*$_POST['delete'];
 	foreach($_POST['del_msgs'] as $id){
 		$ids[]=$id;
-	}
-	if (isset($_COOKIE['ies'])){
+	}*/
+	if (isset($_POST['delete'])&&isset($_COOKIE['ies'])){
 		$user=$_COOKIE['usr'];
-		$_POST['delete'];
+		if(isset($_POST['delete']){
 		foreach($_POST['del_msgs'] as $id){
 			echo $id;
 			$ids[]=$id;
+		}
 		}
 		$link=mysqli_connect("localhost","root","");
 		if(!$link)
