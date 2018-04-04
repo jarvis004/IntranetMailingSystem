58ba6fa9f7f997abbec7f98c064e095b1f922147
Mritunjay Chaudhary <mkdd10@gmail.com>
2018-04-04 23:56:08 +0530

    footer update

--- IES/register.php
+++ IES/register.php
@@ -51,7 +51,7 @@
 		</div>
 	</div>
 </div>
-<div id="footer" ><span class="fn1">&copy; Intranet Email System 2012</span><span class="fn2">powered by:skyroute.org</span></div>
+<div id="footer"><span class="fn1">&copy; Intranet Email System 2018</span><span class="fn2">powered by:IIIT Allahabad</span>
 </div>
 <?php
 	if(count($_POST)>0){
@@ -63,7 +63,7 @@
 			echo("could not select the database");
 		$user=$_POST['uid'];
 		$pass=md5($_POST['pwd']);
-		echo $user."<br/>".$pass;
+		//echo $user."<br/>".$pass;
 		$success=mysql_query("insert into users(username, password) values('$user','$pass');");
 		if($success)
 			echo "<script>alert(\"Registration Successful\");</script>";
